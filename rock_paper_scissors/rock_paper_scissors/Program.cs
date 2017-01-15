61bd3328c52d6281fd652987fcbdc3f59e8ffee5
Jason Edelbrock <synisterj@users.noreply.github.com>
2017-01-15 04:12:41 -0500

    Initial Source Commit
    Added source code that didn't get uploaded in the official initial commit.

---
+++ rock_paper_scissors/rock_paper_scissors/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rock_paper_scissors
+{
+    class Program
+    {
+        static void Main()
+        {
+            GameMenu();
+            while (Console.ReadKey(true).Key != ConsoleKey.E)
+            {
+                StartGame();
+            }
+            ExitGame();
+        }
+        static void GameMenu()
+        {
+            Console.WriteLine("|#####| Rock Paper Scissors |#####|");
+            Console.WriteLine("\nHit Enter to Start New Game");
+        }
+        static void StartGame()
+        {
+            Console.Clear();
+            GameMenu();
+            DateTime timeNow = DateTime.Now;
+            Console.WriteLine("\nNew Game Started " + timeNow);
+            Console.WriteLine("\n\n\rEnter either Rock, Paper or Scissors\n");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("\nYou have chosen " + userInput + "\n");
+            PerformCountdown();
+            string computerInput = ComputerLogic();
+            Console.WriteLine("\nComputer has chosen " + computerInput);
+            DecideWinner(userInput, computerInput);
+        }
+        static void DecideWinner(string user, string computer)
+        {
+            if (user == computer)
+            {
+                Console.WriteLine("\nDraw! Play Again?");
+            }
+            string combineChoices = user + computer;
+            switch (combineChoices)
+            {
+                case "RockPaper":
+                    Lose();
+                    break;
+                case "RockScissors":
+                    Win();
+                    break;
+                case "PaperScissors":
+                    Lose();
+                    break;
+                case "PaperRock":
+                    Win();
+                    break;
+                case "ScissorsRock":
+                    Lose();
+                    break;
+                case "ScissorsPaper":
+                    Win();
+                    break;
+            }
+        }
+        static string ComputerLogic()
+        {
+            string[] winCondition = { "Rock", "Paper", "Scissors" };
+            Random randNum = new Random();
+            int randCondition = randNum.Next(0, 3);
+            return winCondition[randCondition];
+        }
+        static void ExitGame()
+        {
+            int secondCounter = 3;
+            while (secondCounter != 0)
+            {
+                Console.WriteLine("Exiting in " + secondCounter.ToString());
+                System.Threading.Thread.Sleep(1000);
+                secondCounter--;
+            }
+        }
+        static void PerformCountdown()
+        {
+            int secondCounter = 3;
+            while (secondCounter != 0)
+            {
+                Console.WriteLine("Starting in " + secondCounter.ToString());
+                System.Threading.Thread.Sleep(1000);
+                secondCounter--;
+            }
+        }
+        static void Win()
+        {
+            Console.WriteLine("\nYou Win! Play Again?");
+        }
+        static void Lose()
+        {
+            Console.WriteLine("\nYou Lose! Play Again?");
+        }
+    }
+}
---
+++ rock_paper_scissors/rock_paper_scissors/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("rock_paper_scissors")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("rock_paper_scissors")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a629c5b4-3348-40ca-944c-a9923c0a96f9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
